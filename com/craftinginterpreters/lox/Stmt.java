0f6b8607600a3f3d87ee0df0f23c93cae530f899
Atlas Ocean <atlas@xygroup.co>
2023-04-26 04:57:14 -0400

    Chapter 10

--- com/craftinginterpreters/lox/Stmt.java
+++ com/craftinginterpreters/lox/Stmt.java
@@ -6,8 +6,10 @@ abstract class Stmt {
  interface Visitor<R> {
    R visitBlockStmt(Block stmt);
    R visitExpressionStmt(Expression stmt);
+   R visitFunctionStmt(Function stmt);
    R visitIfStmt(If stmt);
    R visitPrintStmt(Print stmt);
+   R visitReturnStmt(Return stmt);
    R visitVarStmt(Var stmt);
    R visitWhileStmt(While stmt);
   }
@@ -34,6 +36,20 @@ abstract class Stmt {
    }
 
     final Expr expression;
+  }
+ static class Function extends Stmt {
+   Function(Token name, List<Token> params,List<Stmt> body) {
+     this.name = name;
+     this.params,List<Stmt> = params,List<Stmt>;
+   }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+       return visitor.visitFunctionStmt(this);
+   }
+
+    final Token name;
+    final List<Token> params,List<Stmt> body;
   }
  static class If extends Stmt {
    If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
@@ -62,6 +78,20 @@ abstract class Stmt {
    }
 
     final Expr expression;
+  }
+ static class Return extends Stmt {
+   Return(Token keyword, Expr value) {
+     this.keyword = keyword;
+     this.value = value;
+   }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+       return visitor.visitReturnStmt(this);
+   }
+
+    final Token keyword;
+    final Expr value;
   }
  static class Var extends Stmt {
    Var(Token name, Expr initializer) {
